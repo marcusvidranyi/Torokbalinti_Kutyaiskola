726dfd7fa3e3bbf2a148ea8b21facbf73cdb2ded
Marcus <vidranyimarcus@gmail.com>
2023-05-06 23:05:45 +0200

    work on subcategory pages continues

--- src/data/categoryData.js
+++ src/data/categoryData.js
@@ -1,27 +1,36 @@
-
+import dog_category_example_1 from "../images/dog_category_example_1.jpg";
+import dog_category_example_2 from "../images/dog_category_example_2.jpg";
+import dog_category_example_3 from "../images/dog_category_example_3.jpg";
+import dog_category_example_4 from "../images/dog_category_example_4.jpg";
+import dog_category_example_5 from "../images/dog_category_example_5.jpg";
 
 const categoryData = {
 
     HOBBI: [
         [
             "PULLER",
-            "PULLER Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida at eros sed consequat. Aenean et dui dictum, interdum libero vitae, volutpat odio. Nullam tincidunt mauris ac justo egestas elementum. Nunc fringilla lectus porta eros venenatis sagittis. Nullam magna eros, malesuada at euismod et, dapibus et mauris. Nam euismod enim mauris, sit amet vehicula lectus tempus at. Proin et neque orci. In elementum felis eget mauris eleifend, vel varius quam suscipit. Curabitur mollis orci dictum, viverra purus vel, bibendum diam. Nulla tincidunt erat molestie sollicitudin venenatis. Praesent ac elementum arcu. Sed placerat tristique fermentum. Vestibulum volutpat sit amet augue a iaculis."
+            "PULLER Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida at eros sed consequat. Aenean et dui dictum, interdum libero vitae, volutpat odio. Nullam tincidunt mauris ac justo egestas elementum. Nunc fringilla lectus porta eros venenatis sagittis. Nullam magna eros, malesuada at euismod et, dapibus et mauris. Nam euismod enim mauris, sit amet vehicula lectus tempus at. Proin et neque orci. In elementum felis eget mauris eleifend, vel varius quam suscipit. Curabitur mollis orci dictum, viverra purus vel, bibendum diam. Nulla tincidunt erat molestie sollicitudin venenatis. Praesent ac elementum arcu. Sed placerat tristique fermentum. Vestibulum volutpat sit amet augue a iaculis.",
+            {dog_category_example_1}
         ],
         [
             "AGILITY",
-            "AGILITY Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida at eros sed consequat. Aenean et dui dictum, interdum libero vitae, volutpat odio. Nullam tincidunt mauris ac justo egestas elementum. Nunc fringilla lectus porta eros venenatis sagittis. Nullam magna eros, malesuada at euismod et, dapibus et mauris. Nam euismod enim mauris, sit amet vehicula lectus tempus at. Proin et neque orci. In elementum felis eget mauris eleifend, vel varius quam suscipit. Curabitur mollis orci dictum, viverra purus vel, bibendum diam. Nulla tincidunt erat molestie sollicitudin venenatis. Praesent ac elementum arcu. Sed placerat tristique fermentum. Vestibulum volutpat sit amet augue a iaculis."
+            "AGILITY Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida at eros sed consequat. Aenean et dui dictum, interdum libero vitae, volutpat odio. Nullam tincidunt mauris ac justo egestas elementum. Nunc fringilla lectus porta eros venenatis sagittis. Nullam magna eros, malesuada at euismod et, dapibus et mauris. Nam euismod enim mauris, sit amet vehicula lectus tempus at. Proin et neque orci. In elementum felis eget mauris eleifend, vel varius quam suscipit. Curabitur mollis orci dictum, viverra purus vel, bibendum diam. Nulla tincidunt erat molestie sollicitudin venenatis. Praesent ac elementum arcu. Sed placerat tristique fermentum. Vestibulum volutpat sit amet augue a iaculis.",
+            {dog_category_example_1}
         ],
         [
             "OBEDIENCE",
-            "OBEDIENCE Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida at eros sed consequat. Aenean et dui dictum, interdum libero vitae, volutpat odio. Nullam tincidunt mauris ac justo egestas elementum. Nunc fringilla lectus porta eros venenatis sagittis. Nullam magna eros, malesuada at euismod et, dapibus et mauris. Nam euismod enim mauris, sit amet vehicula lectus tempus at. Proin et neque orci. In elementum felis eget mauris eleifend, vel varius quam suscipit. Curabitur mollis orci dictum, viverra purus vel, bibendum diam. Nulla tincidunt erat molestie sollicitudin venenatis. Praesent ac elementum arcu. Sed placerat tristique fermentum. Vestibulum volutpat sit amet augue a iaculis."
+            "OBEDIENCE Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida at eros sed consequat. Aenean et dui dictum, interdum libero vitae, volutpat odio. Nullam tincidunt mauris ac justo egestas elementum. Nunc fringilla lectus porta eros venenatis sagittis. Nullam magna eros, malesuada at euismod et, dapibus et mauris. Nam euismod enim mauris, sit amet vehicula lectus tempus at. Proin et neque orci. In elementum felis eget mauris eleifend, vel varius quam suscipit. Curabitur mollis orci dictum, viverra purus vel, bibendum diam. Nulla tincidunt erat molestie sollicitudin venenatis. Praesent ac elementum arcu. Sed placerat tristique fermentum. Vestibulum volutpat sit amet augue a iaculis.",
+            {dog_category_example_1}
         ],
         [
             "IGP",
-            "IGP Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida at eros sed consequat. Aenean et dui dictum, interdum libero vitae, volutpat odio. Nullam tincidunt mauris ac justo egestas elementum. Nunc fringilla lectus porta eros venenatis sagittis. Nullam magna eros, malesuada at euismod et, dapibus et mauris. Nam euismod enim mauris, sit amet vehicula lectus tempus at. Proin et neque orci. In elementum felis eget mauris eleifend, vel varius quam suscipit. Curabitur mollis orci dictum, viverra purus vel, bibendum diam. Nulla tincidunt erat molestie sollicitudin venenatis. Praesent ac elementum arcu. Sed placerat tristique fermentum. Vestibulum volutpat sit amet augue a iaculis."
+            "IGP Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida at eros sed consequat. Aenean et dui dictum, interdum libero vitae, volutpat odio. Nullam tincidunt mauris ac justo egestas elementum. Nunc fringilla lectus porta eros venenatis sagittis. Nullam magna eros, malesuada at euismod et, dapibus et mauris. Nam euismod enim mauris, sit amet vehicula lectus tempus at. Proin et neque orci. In elementum felis eget mauris eleifend, vel varius quam suscipit. Curabitur mollis orci dictum, viverra purus vel, bibendum diam. Nulla tincidunt erat molestie sollicitudin venenatis. Praesent ac elementum arcu. Sed placerat tristique fermentum. Vestibulum volutpat sit amet augue a iaculis.",
+            {dog_category_example_1}
         ],
         [
             "MANTRAILING",
-            "MANTRAILING Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida at eros sed consequat. Aenean et dui dictum, interdum libero vitae, volutpat odio. Nullam tincidunt mauris ac justo egestas elementum. Nunc fringilla lectus porta eros venenatis sagittis. Nullam magna eros, malesuada at euismod et, dapibus et mauris. Nam euismod enim mauris, sit amet vehicula lectus tempus at. Proin et neque orci. In elementum felis eget mauris eleifend, vel varius quam suscipit. Curabitur mollis orci dictum, viverra purus vel, bibendum diam. Nulla tincidunt erat molestie sollicitudin venenatis. Praesent ac elementum arcu. Sed placerat tristique fermentum. Vestibulum volutpat sit amet augue a iaculis."
+            "MANTRAILING Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida at eros sed consequat. Aenean et dui dictum, interdum libero vitae, volutpat odio. Nullam tincidunt mauris ac justo egestas elementum. Nunc fringilla lectus porta eros venenatis sagittis. Nullam magna eros, malesuada at euismod et, dapibus et mauris. Nam euismod enim mauris, sit amet vehicula lectus tempus at. Proin et neque orci. In elementum felis eget mauris eleifend, vel varius quam suscipit. Curabitur mollis orci dictum, viverra purus vel, bibendum diam. Nulla tincidunt erat molestie sollicitudin venenatis. Praesent ac elementum arcu. Sed placerat tristique fermentum. Vestibulum volutpat sit amet augue a iaculis.",
+            {dog_category_example_1}
         ],
     ],
 
@@ -29,23 +38,28 @@ const categoryData = {
     SPORT: [
         [
             "PULLER",
-            "PULLER Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida at eros sed consequat. Aenean et dui dictum, interdum libero vitae, volutpat odio. Nullam tincidunt mauris ac justo egestas elementum. Nunc fringilla lectus porta eros venenatis sagittis. Nullam magna eros, malesuada at euismod et, dapibus et mauris. Nam euismod enim mauris, sit amet vehicula lectus tempus at. Proin et neque orci. In elementum felis eget mauris eleifend, vel varius quam suscipit. Curabitur mollis orci dictum, viverra purus vel, bibendum diam. Nulla tincidunt erat molestie sollicitudin venenatis. Praesent ac elementum arcu. Sed placerat tristique fermentum. Vestibulum volutpat sit amet augue a iaculis."
+            "PULLER Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida at eros sed consequat. Aenean et dui dictum, interdum libero vitae, volutpat odio. Nullam tincidunt mauris ac justo egestas elementum. Nunc fringilla lectus porta eros venenatis sagittis. Nullam magna eros, malesuada at euismod et, dapibus et mauris. Nam euismod enim mauris, sit amet vehicula lectus tempus at. Proin et neque orci. In elementum felis eget mauris eleifend, vel varius quam suscipit. Curabitur mollis orci dictum, viverra purus vel, bibendum diam. Nulla tincidunt erat molestie sollicitudin venenatis. Praesent ac elementum arcu. Sed placerat tristique fermentum. Vestibulum volutpat sit amet augue a iaculis.",
+            {dog_category_example_1}
         ],
         [
             "AGILITY",
-            "AGILITY Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida at eros sed consequat. Aenean et dui dictum, interdum libero vitae, volutpat odio. Nullam tincidunt mauris ac justo egestas elementum. Nunc fringilla lectus porta eros venenatis sagittis. Nullam magna eros, malesuada at euismod et, dapibus et mauris. Nam euismod enim mauris, sit amet vehicula lectus tempus at. Proin et neque orci. In elementum felis eget mauris eleifend, vel varius quam suscipit. Curabitur mollis orci dictum, viverra purus vel, bibendum diam. Nulla tincidunt erat molestie sollicitudin venenatis. Praesent ac elementum arcu. Sed placerat tristique fermentum. Vestibulum volutpat sit amet augue a iaculis."
+            "AGILITY Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida at eros sed consequat. Aenean et dui dictum, interdum libero vitae, volutpat odio. Nullam tincidunt mauris ac justo egestas elementum. Nunc fringilla lectus porta eros venenatis sagittis. Nullam magna eros, malesuada at euismod et, dapibus et mauris. Nam euismod enim mauris, sit amet vehicula lectus tempus at. Proin et neque orci. In elementum felis eget mauris eleifend, vel varius quam suscipit. Curabitur mollis orci dictum, viverra purus vel, bibendum diam. Nulla tincidunt erat molestie sollicitudin venenatis. Praesent ac elementum arcu. Sed placerat tristique fermentum. Vestibulum volutpat sit amet augue a iaculis.",
+            {dog_category_example_2}
         ],
         [
-            "OBEDIENCE",
-            "OBEDIENCE Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida at eros sed consequat. Aenean et dui dictum, interdum libero vitae, volutpat odio. Nullam tincidunt mauris ac justo egestas elementum. Nunc fringilla lectus porta eros venenatis sagittis. Nullam magna eros, malesuada at euismod et, dapibus et mauris. Nam euismod enim mauris, sit amet vehicula lectus tempus at. Proin et neque orci. In elementum felis eget mauris eleifend, vel varius quam suscipit. Curabitur mollis orci dictum, viverra purus vel, bibendum diam. Nulla tincidunt erat molestie sollicitudin venenatis. Praesent ac elementum arcu. Sed placerat tristique fermentum. Vestibulum volutpat sit amet augue a iaculis."
+            "ŐRZŐ-VÉDŐ",
+            "ŐRZŐ-VÉDŐ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida at eros sed consequat. Aenean et dui dictum, interdum libero vitae, volutpat odio. Nullam tincidunt mauris ac justo egestas elementum. Nunc fringilla lectus porta eros venenatis sagittis. Nullam magna eros, malesuada at euismod et, dapibus et mauris. Nam euismod enim mauris, sit amet vehicula lectus tempus at. Proin et neque orci. In elementum felis eget mauris eleifend, vel varius quam suscipit. Curabitur mollis orci dictum, viverra purus vel, bibendum diam. Nulla tincidunt erat molestie sollicitudin venenatis. Praesent ac elementum arcu. Sed placerat tristique fermentum. Vestibulum volutpat sit amet augue a iaculis.",
+            {dog_category_example_3}
         ],
         [
             "IGP",
-            "IGP Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida at eros sed consequat. Aenean et dui dictum, interdum libero vitae, volutpat odio. Nullam tincidunt mauris ac justo egestas elementum. Nunc fringilla lectus porta eros venenatis sagittis. Nullam magna eros, malesuada at euismod et, dapibus et mauris. Nam euismod enim mauris, sit amet vehicula lectus tempus at. Proin et neque orci. In elementum felis eget mauris eleifend, vel varius quam suscipit. Curabitur mollis orci dictum, viverra purus vel, bibendum diam. Nulla tincidunt erat molestie sollicitudin venenatis. Praesent ac elementum arcu. Sed placerat tristique fermentum. Vestibulum volutpat sit amet augue a iaculis."
+            "IGP Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida at eros sed consequat. Aenean et dui dictum, interdum libero vitae, volutpat odio. Nullam tincidunt mauris ac justo egestas elementum. Nunc fringilla lectus porta eros venenatis sagittis. Nullam magna eros, malesuada at euismod et, dapibus et mauris. Nam euismod enim mauris, sit amet vehicula lectus tempus at. Proin et neque orci. In elementum felis eget mauris eleifend, vel varius quam suscipit. Curabitur mollis orci dictum, viverra purus vel, bibendum diam. Nulla tincidunt erat molestie sollicitudin venenatis. Praesent ac elementum arcu. Sed placerat tristique fermentum. Vestibulum volutpat sit amet augue a iaculis.",
+            {dog_category_example_4}
         ],
         [
             "MANTRAILING",
-            "MANTRAILING Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida at eros sed consequat. Aenean et dui dictum, interdum libero vitae, volutpat odio. Nullam tincidunt mauris ac justo egestas elementum. Nunc fringilla lectus porta eros venenatis sagittis. Nullam magna eros, malesuada at euismod et, dapibus et mauris. Nam euismod enim mauris, sit amet vehicula lectus tempus at. Proin et neque orci. In elementum felis eget mauris eleifend, vel varius quam suscipit. Curabitur mollis orci dictum, viverra purus vel, bibendum diam. Nulla tincidunt erat molestie sollicitudin venenatis. Praesent ac elementum arcu. Sed placerat tristique fermentum. Vestibulum volutpat sit amet augue a iaculis."
+            "MANTRAILING Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida at eros sed consequat. Aenean et dui dictum, interdum libero vitae, volutpat odio. Nullam tincidunt mauris ac justo egestas elementum. Nunc fringilla lectus porta eros venenatis sagittis. Nullam magna eros, malesuada at euismod et, dapibus et mauris. Nam euismod enim mauris, sit amet vehicula lectus tempus at. Proin et neque orci. In elementum felis eget mauris eleifend, vel varius quam suscipit. Curabitur mollis orci dictum, viverra purus vel, bibendum diam. Nulla tincidunt erat molestie sollicitudin venenatis. Praesent ac elementum arcu. Sed placerat tristique fermentum. Vestibulum volutpat sit amet augue a iaculis.",
+            {dog_category_example_5}
         ],
     ],
 
@@ -55,13 +69,30 @@ const categoryData = {
     SZOLGÁLATI: [
         [
             "ŐRZŐ-VÉDŐ",
-            "ŐRZŐ-VÉDŐ Lorem Ipsum"
+            "ŐRZŐ-VÉDŐ Lorem Ipsum",
+            {dog_category_example_1}
         ],
         [
             "ENGEDELMES",
-            "ENGEDELMES Lorem Ipsum"
+            "ENGEDELMES Lorem Ipsum",
+            {dog_category_example_1}
         ],
-        
+        [
+            "ENGEDELMES",
+            "ENGEDELMES Lorem Ipsum",
+            {dog_category_example_1}
+        ],
+        [
+            "ENGEDELMES",
+            "ENGEDELMES Lorem Ipsum",
+            {dog_category_example_1}
+        ],
+        [
+            "ENGEDELMES",
+            "ENGEDELMES Lorem Ipsum",
+            {dog_category_example_1}
+        ],
+
     ],
 
 };
